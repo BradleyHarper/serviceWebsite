6a282314af6bf1a07568de23979065e42ffd55bb
bradleyharper <bradharper123@hotmail.com>
2020-09-08 08:58:22 -0500

    added angular fontawesome icons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { faPhone, faMapPin, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [FontAwesomeModule]
 })
 export class AppComponent {
-  title = 'harpService';
+  faphone = faPhone;
+  faMapPin = faMapPin;
+  faEnvelope = faEnvelope;
 }
