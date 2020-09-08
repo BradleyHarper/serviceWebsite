26b40f26b00e407bc8ca929ce9d4edc80db8befc
bradleyharper <bradharper123@hotmail.com>
2020-09-08 17:33:09 -0500

    Created an email for the business. better looking footer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { faPhone, faMapPin, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,14 @@ export class AppComponent implements OnInit {
   faphone = faPhone;
   faMapPin = faMapPin;
   faEnvelope = faEnvelope;
+  options = {};
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    document.addEventListener('DOMContentLoaded', () => {
+      const elems = document.querySelectorAll('.parallax');
+      const instances = M.Parallax.init(elems, this.options);
+    });
+  }
 }
