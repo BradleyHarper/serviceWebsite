a4abdca49249712140322afb15373883e0815d0f
bradleyharper <bradharper123@hotmail.com>
2020-09-08 11:16:34 -0500

    added more svg icons, sources button, other style changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { faPhone, faMapPin, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,17 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
   styleUrls: ['./app.component.scss'],
   providers: [FontAwesomeModule]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   faphone = faPhone;
   faMapPin = faMapPin;
   faEnvelope = faEnvelope;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    document.addEventListener('DOMContentLoaded', () => {
+      const elems = document.querySelectorAll('.dropdown-trigger');
+      const instances = M.Dropdown.init(elems);
+    });
+  }
 }
