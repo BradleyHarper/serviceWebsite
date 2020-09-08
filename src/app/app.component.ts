0ba5ecd6bc484d9e6f0ee3b46e9791d8744f1aac
bradleyharper <bradharper123@hotmail.com>
2020-09-08 14:07:47 -0500

    Created google map of delivery area. Got rid of the button for the sources I used to create the site.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { faPhone, faMapPin, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
@@ -16,10 +15,5 @@ export class AppComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-    document.addEventListener('DOMContentLoaded', () => {
-      const elems = document.querySelectorAll('.dropdown-trigger');
-      const instances = M.Dropdown.init(elems);
-    });
-  }
+  ngOnInit(): void { }
 }
