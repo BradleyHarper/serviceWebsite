0e1dcfb92b2ae186081f9f6f67894eb7197535b4
bradleyharper <bradharper123@hotmail.com>
2020-09-09 10:49:43 -0500

    Ran all the content through an ngfor loop to work better with my new staggered style choice. Used a lighter font weight as well for more than half the application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faPhone, faMapPin, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Content } from './models/content';
 import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [FontAwesomeModule]
+  providers: [FontAwesomeModule, Content]
 })
 export class AppComponent implements OnInit {
+  descriptions = new Content();
   faphone = faPhone;
   faMapPin = faMapPin;
   faEnvelope = faEnvelope;
@@ -17,7 +19,7 @@ export class AppComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): any {
     document.addEventListener('DOMContentLoaded', () => {
       const elems = document.querySelectorAll('.parallax');
       const instances = M.Parallax.init(elems, this.options);
---
+++ src/app/models/content.ts
@@ -0,0 +1,43 @@
+export class Content {
+  data = [
+    {
+      title: 'About',
+      content: 'I\'ve worked as a professional small engine repair man for a few years but I\'ve been around these things all my life.\n' +
+        '      I always ensure the customer leaves with a piece of equipment that is fully functional. If I have any issues with the repair I make it\n' +
+        '      100% clear to keep the customer in the loop on what is going on. My goal is to have you fully aware of what\'s going on, perform a quality repair,\n' +
+        '      and all weeks quicker than the local shops! Same day diagnosis'
+    },
+    {
+      title: 'Equipment',
+      content: 'Tillers, snow blowers, string trimmers, push mowers, riding lawn mowers, gokarts, minibikes.From brands ranging from toro, stihl, redmax, briggs and stratton, honda, walmart brands, and more.'
+    },
+    {
+      title: 'Contact for quote & appointment',
+      content: 'Contact me using email or phone provided below and we\'ll schedule a good time for me to help.'
+    },
+    {
+      title: 'testing...',
+      content: 'asldfa;sdlfkjas;ldfkjas;ldkfja;sldkfajsdkfjal;skdjfljasd;lfkjas;ldfkja;sldfkja;slkdfj;alsdkfja'
+    },
+    {
+      title: 'testing...',
+      content: 'asldfa;sdlfkjas;ldfkjas;ldkfja;sldkfajsdkfjal;skdjfljasd;lfkjas;ldfkja;sldfkja;slkdfj;alsdkfja'
+    },
+    {
+      title: 'testing...',
+      content: 'asldfa;sdlfkjas;ldfkjas;ldkfja;sldkfajsdkfjal;skdjfljasd;lfkjas;ldfkja;sldfkja;slkdfj;alsdkfja'
+    },
+    {
+      title: 'testing...',
+      content: 'asldfa;sdlfkjas;ldfkjas;ldkfja;sldkfajsdkfjal;skdjfljasd;lfkjas;ldfkja;sldfkja;slkdfj;alsdkfja'
+    },
+    {
+      title: 'testing...',
+      content: 'asldfa;sdlfkjas;ldfkjas;ldkfja;sldkfajsdkfjal;skdjfljasd;lfkjas;ldfkja;sldfkja;slkdfj;alsdkfja'
+    },
+    {
+      title: 'testing...',
+      content: 'asldfa;sdlfkjas;ldfkjas;ldkfja;sldkfajsdkfjal;skdjfljasd;lfkjas;ldfkja;sldfkja;slkdfj;alsdkfja'
+    },
+  ];
+}
