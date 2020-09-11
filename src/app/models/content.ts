cdf36d7dff10934e3e6b15ec507820df856fb5ca
bradleyharper <bradharper123@hotmail.com>
2020-09-11 11:34:34 -0500

    Fixed grammar issues in content. made links the default color so people know to click on them.

--- src/app/models/content.ts
+++ src/app/models/content.ts
@@ -2,36 +2,36 @@ export class Content {
   data = [
     {
       title: 'About Harper Auto',
-      content: 'Hello! My name is Bradley Harper and Professional mobile small engine repair is my specialty. My goal is to have you fully aware of the repair process / progress, perform a quality repair, ' +
-        '      Same day diagnosis'
+      content: 'Hello! My name is Bradley Harper and I\'m a professional small engine mechanic offering mobile services. My goal is to have you fully aware of the repair process, perform a quality repair, ' +
+        '      and with a same day diagnosis.'
     },
     {
       title: 'Equipment',
-      content: 'I repair Tillers, snow blowers, string trimmers, push mowers, riding lawn mowers, gokarts, minibikes, dirtbikes. From brands ranging from toro, stihl, redmax, briggs and stratton, honda, walmart brands, and more.'
-    },
-    {
-      title: 'Quote & appointment',
-      content: 'Schedule a time or tap the phone number at the bottom of the website if you have any questions to ask me directly or book a time for me to come help with your small engine needs. ' +
-        'I will follow up with a phone call if you schedule an appointment to see if my services would be the best for you.'
+      content: 'I repair anything with a pull cord. From brands ranging from Toro, Stihl, Redmax, Briggs and Stratton, Honda, Craftsman, and more.'
     },
     {
       title: 'Pricing',
       content: 'On booking you\'ll see my rate per hour(s). If the job only takes an hour, you pay an hour. The final bill will have the cost of parts included.'
     },
+    // {
+    //   title: 'First clients',
+    //   content: 'Although I\'ve been repairing small engine ran equipment for years being mobile is brand new to me. ' +
+    //     'If the job takes less or more time than expected I\'ll contact you and keep you informed on the updated completion time.'
+    // },
     {
-      title: 'First clients',
-      content: 'Although I\'ve been repairing small engine ran equipment for years being mobile is brand new to me, ' +
-        'If the job takes less or more time than expected I\'ll contact you and keep you informed on the updated completion time.'
-    },
-    {
-      title: 'What kind of repairs',
-      content: 'Ran over a big obstruction in your yard with your push mower and now it backfires and wont start? Small engine bogging / wont run / doesn\'t rev up to operating speed?' +
+      title: 'Repair Types',
+      content: 'Hard to start hand held equipment / push mower wearing out your shoulder? Self propelled drive on push mower not working anymore?' +
         ' Give me a call.'
     },
     {
       title: 'Location',
       content: 'To make sure I\'m close enough to you, click on "check location" at the bottom of the website.' +
-        ' If you think you\'re not too far away from my residence but out of the service zone on the map give me a call as the map is till a work in progress.'
+        ' If you\'re not too far away from my residence but out of the service zone on the map give me a call.'
+    },
+    {
+      title: 'Quote & Appointment',
+      content: 'If you have any questions to ask me directly via phone or book a time for me to come help with your small engine needs. ' +
+        'I will follow up with a phone call if you schedule an appointment to see if my services would be the best for you.'
     },
     {
       title: 'Questions',
