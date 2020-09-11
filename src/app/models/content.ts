9abc592b4d1495bfd79c5a48a3220a37883d33d8
bradleyharper <bradharper123@hotmail.com>
2020-09-10 20:15:53 -0500

    ready for v1.0 prod.

--- src/app/models/content.ts
+++ src/app/models/content.ts
@@ -1,31 +1,41 @@
 export class Content {
   data = [
     {
-      title: 'About',
-      content: 'I\'ve worked as a professional small engine repair man for a few years but I\'ve been around these things all my life.\n' +
-        '      I ensure the customer leaves with a piece of equipment that is fully functional. If I have any issues with the repair I make it\n' +
-        '      100% clear to keep the customer in the loop on what is going on. My goal is to have you fully aware of what\'s going on, perform a quality repair,\n' +
-        '      and all weeks quicker than the local shops! Same day diagnosis'
+      title: 'About Harper Auto',
+      content: 'Hello! My name is Bradley Harper and Professional mobile small engine repair is my specialty. My goal is to have you fully aware of the repair process / progress, perform a quality repair, ' +
+        '      Same day diagnosis'
     },
     {
       title: 'Equipment',
-      content: 'Tillers, snow blowers, string trimmers, push mowers, riding lawn mowers, gokarts, minibikes.From brands ranging from toro, stihl, redmax, briggs and stratton, honda, walmart brands, and more.'
+      content: 'I repair Tillers, snow blowers, string trimmers, push mowers, riding lawn mowers, gokarts, minibikes, dirtbikes. From brands ranging from toro, stihl, redmax, briggs and stratton, honda, walmart brands, and more.'
     },
     {
-      title: 'Contact for quote & appointment',
-      content: 'Contact me using email or phone provided below and we\'ll schedule a good time for me to help.'
+      title: 'Quote & appointment',
+      content: 'Schedule a time or tap the phone number at the bottom of the website if you have any questions to ask me directly or book a time for me to come help with your small engine needs. ' +
+        'I will follow up with a phone call if you schedule an appointment to see if my services would be the best for you.'
     },
     {
-      title: 'testing...',
-      content: 'asldfa;sdlfkjas;ldfkjas;ldkfja;sldkfajsdkfjal;skdjfljasd;lfkjas;ldfkja;sldfkja;slkdfj;alsdkfja'
+      title: 'Pricing',
+      content: 'On booking you\'ll see my rate per hour(s). If the job only takes an hour, you pay an hour. The final bill will have the cost of parts included.'
     },
     {
-      title: 'testing...',
-      content: 'asldfa;sdlfkjas;ldfkjas;ldkfja;sldkfajsdkfjal;skdjfljasd;lfkjas;ldfkja;sldfkja;slkdfj;alsdkfja'
+      title: 'First clients',
+      content: 'Although I\'ve been repairing small engine ran equipment for years being mobile is brand new to me, ' +
+        'If the job takes less or more time than expected I\'ll contact you and keep you informed on the updated completion time.'
     },
     {
-      title: 'testing...',
-      content: 'asldfa;sdlfkjas;ldfkjas;ldkfja;sldkfajsdkfjal;skdjfljasd;lfkjas;ldfkja;sldfkja;slkdfj;alsdkfja'
+      title: 'What kind of repairs',
+      content: 'Ran over a big obstruction in your yard with your push mower and now it backfires and wont start? Small engine bogging / wont run / doesn\'t rev up to operating speed?' +
+        ' Give me a call.'
+    },
+    {
+      title: 'Location',
+      content: 'To make sure I\'m close enough to you, click on "check location" at the bottom of the website.' +
+        ' If you think you\'re not too far away from my residence but out of the service zone on the map give me a call as the map is till a work in progress.'
+    },
+    {
+      title: 'Questions',
+      content: 'Feel free to reach out via phone or email located below. This is the first rendition of the website, If you find errors please let me know. Thank you!'
     }
   ];
 }
