9444f163bd9f6720497d07f600b7d542419605a2
bradleyharper <bradharper123@hotmail.com>
2020-09-13 19:24:20 -0500

    Google map now loads my delivery map instead of loading all the way zoomed out.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { faPhone, faMapPin, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Content } from './models/content';
-import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
@@ -10,22 +9,14 @@ import * as M from 'materialize-css';
   styleUrls: ['./app.component.scss'],
   providers: [FontAwesomeModule, Content]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   descriptions = new Content();
   faPhone = faPhone;
   faMapPin = faMapPin;
   faEnvelope = faEnvelope;
   mapVisible = true;
 
-  constructor() { }
-
-  ngOnInit(): any { }
-
   openMapToggle = () => {
     this.mapVisible = !this.mapVisible;
-  }
-
-  closeMap = () => {
-    this.mapVisible = false;
   }
 }
